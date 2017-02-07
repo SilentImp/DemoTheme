527315d03c539cb46c5e606bad4aa837e4e60e91
Anton Nemtsev <thesilentimp@gmail.com>
2017-02-07 14:09:03 +0200

    name change

--- registration.php
+++ registration.php
@@ -6,6 +6,6 @@
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::THEME,
-    'frontend/DemoVendor/DemoTheme',
+    'frontend/demo-vendor/demo-theme',
     __DIR__
 );
